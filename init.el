0de86e118ec031fd52375a40a30245706e95aab7
qingrongfu <qingrongfu@tencent.com>
2016-10-17 22:36:08 +0800

    add plugin

--- init.el
+++ init.el
@@ -5,9 +5,32 @@
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
 (package-initialize)
 
+;; 模板设置
+(load-theme 'wombat t)
+;; 语法检查
+(global-font-lock-mode 1)
+;; 关闭开始页面
+(setq inhibit-startup-message t)
+(setq gnus-inhibit-startup-message t)
+
 (require 'evil)
 (evil-mode 1)
 
 (add-to-list 'load-path "~/.emacs.d/myemacs/elpa-mirror")
 (require 'elpa-mirror)
 (setq elpamr-default-output-directory "~/.emacs.d/myemacs/myelpa")
+
+;; php mode and php syntax check
+(require 'php-mode)
+(require 'flymake-php)
+(add-hook 'php-mode-hook 'flymake-php-load)
+
+;; auto-complate
+(ac-config-default)
+
+(autoload
+    'ace-jump-mode
+    "ace-jump-mode"
+    "Emacs quick move minor mode"
+    t)
+(define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
