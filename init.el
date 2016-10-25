888ae86ee1c71273e8c8c5f3bc4379ec93a7a946
qingrongfu <qingrongfu@tencent.com>
2016-10-25 23:24:10 +0800

    add org-mode

--- init.el
+++ init.el
@@ -34,3 +34,10 @@
     "Emacs quick move minor mode"
     t)
 (define-key global-map (kbd "C-c SPC") 'ace-jump-mode)
+
+;; The following lines are always needed. Choose your own keys.
+(add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
+(add-hook 'org-mode-hook 'turn-on-font-lock) ; not needed when global-font-lock-mode is on
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
